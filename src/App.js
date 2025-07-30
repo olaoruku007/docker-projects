f8926534c3d74c812033420a1c901cce30a46674
olaoruku007 <93499206+olaoruku007@users.noreply.github.com>
2025-07-30 12:27:36 -0400

    Update1 App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
-          Congratulations sir! App has successfully been compiled.
+          App dockerized, compiled and deployed by zthcloud.
         </h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
